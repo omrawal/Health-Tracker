f884e61b279b9159dc6fc2888c20a60eafbbba8f
Om Rawal <omrawal007@gmail.com>
2021-10-09 11:09:53 +0530

    API created

---
+++ api_calls.py
@@ -0,0 +1,42 @@
+import requests
+
+url = "https://fitness-api.p.rapidapi.com/fitness"
+
+
+headers = {
+    'content-type': "application/x-www-form-urlencoded",
+    'x-rapidapi-key': "1028412bafmsh4e76670100a15dep1e2198jsn571d5dd04aef",
+    'x-rapidapi-host': "fitness-api.p.rapidapi.com"
+}
+
+
+def getStats(heightInCms, WeightInKg, age, gender='male'):
+    payload = "height="+str(heightInCms)+"&weight=" + \
+        str(WeightInKg)+"&age="+str(age)+"&gender="+str(gender)
+
+    response = requests.request(
+        "POST", url, data=payload, headers=headers).json()
+    # print(response)
+    min_range = min(response['idealBodyWeight']['hamwi']['metric']['value'],
+                    response['idealBodyWeight']['devine']['metric']['value'],
+                    response['idealBodyWeight']['robinson']['metric']['value'],
+                    response['idealBodyWeight']['miller']['metric']['value'])-10
+
+    max_range = max(response['idealBodyWeight']['hamwi']['metric']['value'],
+                    response['idealBodyWeight']['devine']['metric']['value'],
+                    response['idealBodyWeight']['robinson']['metric']['value'],
+                    response['idealBodyWeight']['miller']['metric']['value'])
+
+    final_stats = {'height': response['height'],
+                   'weight': response['weight'],
+                   'age': response['age'],
+                   'gender': response['gender'],
+                   'BMI': str(response['bodyMassIndex']['value'])+' '+response['bodyMassIndex']['unit'],
+                   'conclusion': response['bodyMassIndex']['conclusion'],
+                   'idealWeightRange': str(min_range)+" kg - "+str(max_range)+" kg"
+                   }
+    # print(final_stats)
+    return(final_stats)
+
+
+print(getStats(183, 89, 21, 'male'))
